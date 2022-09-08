09ff06f7e1dc9c3c2454aea2395b6deceac8e43a
Drakepal <playonplay23@gmail.com>
2022-09-08 13:39:34 -0500

    MenuHeaderView Done

---
+++ WeatherForecasting/View/MenuHeaderView.swift
@@ -0,0 +1,48 @@
+//
+//  MenuHeaderView.swift
+//  WeatherForecasting
+//
+//  Created by PM Student on 9/8/22.
+//
+
+import SwiftUI
+
+struct MenuHeaderView: View {
+    
+    @State private var searchTerm = "Tulsa"
+    
+    var body: some View {
+        HStack {
+            TextField("", text: $searchTerm)
+                .padding(.leading, 20)
+            
+            Button{
+                
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.blue)
+                    
+                    Image(systemName: "location.fill")
+                }
+            }
+            .frame(width: 50, height: 50)
+        }
+        .foregroundColor(.white)
+        .padding()
+        .background(ZStack (alignment: .leading){
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(.white)
+                .padding(.leading, 10)
+            
+                RoundedRectangle(cornerRadius: 10)
+                .fill(Color.blue.opacity(0.5))
+        })
+    }
+}
+
+struct MenuHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuHeaderView()
+    }
+}
---
+++ WeatherForecasting/WeatherForecastingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastingApp.swift
+//  WeatherForecasting
+//
+//  Created by PM Student on 9/8/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
