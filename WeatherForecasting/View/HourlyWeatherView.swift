ed1697c745ebc16ecc9410addaa509db47638e81
Drakepal <playonplay23@gmail.com>
2022-09-12 14:27:31 -0500

    HourlyWeatherView done

---
+++ WeatherForecasting/View/HourlyWeatherView.swift
@@ -0,0 +1,60 @@
+//
+//  HourlyWeatherView.swift
+//  WeatherForecasting
+//
+//  Created by PM Student on 9/12/22.
+//
+
+import SwiftUI
+
+struct HourlyWeatherView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 80) {
+                VStack(spacing: 20) {
+                    Image(systemName: "sun.max.fill")
+                    Text("Hour")
+                    Text("Temp")
+                }
+                VStack(spacing: 20) {
+                    Image(systemName: "sun.max.fill")
+                    Text("Hour")
+                    Text("Temp")
+                }
+                VStack(spacing: 20) {
+                    Image(systemName: "sun.max.fill")
+                    Text("Hour")
+                    Text("Temp")
+                }
+                VStack(spacing: 20) {
+                    Image(systemName: "sun.max.fill")
+                    Text("Hour")
+                    Text("Temp")
+                }
+            }
+        }
+    }
+    private func getHourlyView(hour: String, image: Image, temp: String) -> some View {
+        VStack(spacing: 20) {
+            Text(hour)
+            image
+                .foregroundColor(.yellow)
+            Text(temp)
+        }
+        .foregroundColor(.white)
+        .padding()
+        .background(RoundedRectangle(cornerRadius: 5).fill(LinearGradient(gradient: Gradient(colors: [Color("Color1"), Color("Color2")]), startPoint: .topLeading, endPoint: .bottomTrailing)))
+        .shadow(color: Color.white.opacity(0.1), radius: 2, x: -2, y: -2)
+        .shadow(color: Color.black.opacity(0.2), radius: 2, x: 2, y: 2)
+    }
+}
+
+
+
+
+
+struct HourlyWeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        HourlyWeatherView()
+    }
+}
