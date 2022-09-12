9b74350238912cc25f4a299fa54dbce201bcfb39
Drakepal <playonplay23@gmail.com>
2022-09-12 14:40:52 -0500

    cityview is done

---
+++ WeatherForecasting/View/CityView.swift
@@ -0,0 +1,22 @@
+//
+//  CityView.swift
+//  WeatherForecasting
+//
+//  Created by PM Student on 9/12/22.
+//
+
+import SwiftUI
+
+struct CityView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct CityView_Previews: PreviewProvider {
+    static var previews: some View {
+        CityView()
+    }
+}
