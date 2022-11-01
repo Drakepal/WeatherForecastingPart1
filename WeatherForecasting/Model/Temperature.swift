4217df5cce4b249420d5f5a98dd58a34533301d5
Drakepal <playonplay23@gmail.com>
2022-11-01 13:34:15 -0500

    Temperature struct is done

---
+++ WeatherForecasting/Model/Temperature.swift
@@ -0,0 +1,13 @@
+//
+//  Temperature.swift
+//  WeatherForecasting
+//
+//  Created by PM Student on 11/1/22.
+//
+
+import Foundation
+
+struct Temperature: Codable {
+    var min: Double
+    var max: Double
+}
