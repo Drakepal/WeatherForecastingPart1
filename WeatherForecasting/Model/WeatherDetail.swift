72f572eaed7d4082debeecde21ce9c0e1889c72a
Drakepal <playonplay23@gmail.com>
2022-11-01 13:32:15 -0500

    WeatherDetail is done

---
+++ WeatherForecasting/Model/WeatherDetail.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherDetail.swift
+//  WeatherForecasting
+//
+//  Created by PM Student on 11/1/22.
+//
+
+import Foundation
+
+struct WeatherDetail: Codable {
+    var main: String
+    var description: String
+    var icon: String
+}
