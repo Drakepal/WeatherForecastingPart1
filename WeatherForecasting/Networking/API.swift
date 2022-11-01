b6d81f70d591991ff24e15724548e2f75eac80eb
Drakepal <playonplay23@gmail.com>
2022-11-01 12:36:56 -0500

    API struct is done

---
+++ WeatherForecasting/Networking/API.swift
@@ -0,0 +1,13 @@
+//
+//  API.swift
+//  WeatherForecasting
+//
+//  Created by PM Student on 11/1/22.
+//
+
+import Foundation
+
+
+struct API {
+    static let key = "f7cb0f773b8db7e0d1ee90304559477e"
+}
